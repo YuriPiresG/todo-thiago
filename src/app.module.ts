0694b9c29c902c704e0bfd8bca4ef2aabc8e2053
Yuri Pires <yuripiresbr@gmail.com>
2023-05-27 12:57:49 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todo/entities/todo.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin123',
+      database: 'todoP2',
+      entities: [Todo],
+      synchronize: true,
+    }),
+    TodoModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
